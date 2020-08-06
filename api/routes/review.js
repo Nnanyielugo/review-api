c7c15f13d587c9a11da890b231653fc3aee18b5a
Nnanyielugo <kz.onyegbule@gmail.com>
2020-08-06 23:12:44 +0100

    routes definitions and method skeletons for review comments

--- api/routes/review.js
+++ api/routes/review.js
@@ -1,13 +1,24 @@
 const router = require('express').Router();
 const reviews = require('../services/review');
+const comments = require('../services/comments');
 const auth = require('../middleware/auth');
 
 router.param('review', reviews.preloadReview);
+router.param('comment', comments.preloadComment);
 
 router.get('/', auth.optional, reviews.list);
 router.get('/review', auth.optional, reviews.get);
 router.post('/', auth.required, reviews.create);
 router.patch('/:review', auth.required, reviews.update);
 router.delete('/:review', auth.required, reviews.delete);
+router.post('/:review/favorite', auth.required, reviews.favorite);
+router.delete('/:review/unfavorite', auth.required, reviews.unfavorite);
+
+router.get('/:review/comments', auth.optional, comments.get);
+router.post('/:review/comments', auth.required, comments.create);
+router.patch('/:review/comments/:comment', auth.required, comments.update);
+router.delete('/:review/comment/:comment', auth.required, comments.delete);
+router.post('/:review/comments/:comment/favorite', auth.required, comments.favorite);
+router.delete('/:review/comments/:comment/unfavorite', auth.required, comments.unfavorite);
 
 module.exports = router;
---
+++ api/services/comments.js
@@ -0,0 +1,13 @@
+exports.preloadComment = function (req, res, next, id) {};
+
+exports.get = function (req, res, next) {};
+
+exports.create = function (req, res, next) {};
+
+exports.update = function (req, res, next) {};
+
+exports.delete = function (req, res, next) {};
+
+exports.favorite = function (req, res, next) {};
+
+exports.unfavorite = function (req, res, next) {};

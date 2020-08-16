bb109be45bf463d758742b8b560d0a856a2247ea
Nnanyielugo <kz.onyegbule@gmail.com>
2020-08-16 12:42:35 +0100

    add suspend user route

--- api/routes/user.js
+++ api/routes/user.js
@@ -6,5 +6,6 @@ router.post('/', user.signup);
 router.post('/login', user.login);
 router.get('/user', auth.required, user.get);
 router.patch('/user', auth.required, user.update);
+router.post('/user/suspend', auth.required, user.suspend);
 
 module.exports = router;

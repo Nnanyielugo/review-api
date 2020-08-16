a033c8b45a8ae355a1a8cab2c4c6ca98f3771dc6
Nnanyielugo <kz.onyegbule@gmail.com>
2020-08-16 12:40:52 +0100

    refactor user services away from using Promise.then()

--- api/services/user.js
+++ api/services/user.js
@@ -1,56 +1,56 @@
 const User = require('mongoose').model('User');
-
 const passport = require('passport');
 
-module.exports.get = (req, res, next) => {
-  User
-    .findById(req.payload.id)
-    .then((user) => {
-      if (!user) {
-        return res.sendStatus(401);
-      }
-      if (user.suspended && user.suspension_timeline > Date.now()) {
-        return user
-          .update({
-            suspended: false,
-            suspension_timeline: null,
-          })
-          .then(() => res.json({ user: user.toAuthJsonFor() }))
-          .catch(next);
-      }
+module.exports.get = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.payload.id);
+    if (!user) {
+      return res.sendStatus(401);
+    }
+
+    if (user.suspended && user.suspension_timeline > Date.now()) {
+      await user.update({
+        suspended: false,
+        suspension_timeline: null,
+      });
+
       return res.json({ user: user.toAuthJsonFor() });
-    })
-    .catch(next);
+    }
+    return res.json({ user: user.toAuthJsonFor() });
+  } catch (err) {
+    next(err);
+  }
 };
 
-module.exports.signup = (req, res, next) => {
-  const user = new User();
-  user.username = req.body.user.username;
-  user.email = req.body.user.email;
-  user.first_name = req.body.user.first_name;
-  user.family_name = req.body.user.family_name;
-  user.setPassword(req.body.user.password);
+module.exports.signup = async (req, res, next) => {
+  try {
+    const user = new User();
+    user.username = req.body.user.username;
+    user.email = req.body.user.email;
+    user.first_name = req.body.user.first_name;
+    user.family_name = req.body.user.family_name;
+    user.setPassword(req.body.user.password);
 
+    if (
+      !req.body.user.username
+        || !req.body.user.password
+        || !req.body.user.email
+        || !req.body.user.first_name
+        || !req.body.user.family_name
+    ) {
+      // generic error message since there will be frontend validation
+      return res.status(400).json({
+        error: {
+          message: 'Required form values need to be complete!',
+        },
+      });
+    }
 
-  if (
-    !req.body.user.username
-      || !req.body.user.password
-      || !req.body.user.email
-      || !req.body.user.first_name
-      || !req.body.user.family_name
-  ) {
-    // generic error message since there will be frontend validation
-    return res.status(400).json({
-      error: {
-        message: 'Required form values need to be complete!',
-      },
-    });
+    await user.save();
+    return res.json({ user: user.toAuthJsonFor() });
+  } catch (err) {
+    next(err);
   }
-
-  return user
-    .save()
-    .then(() => res.json({ user: user.toAuthJsonFor() }))
-    .catch(next);
 };
 
 module.exports.login = (req, res, next) => {
@@ -74,36 +74,35 @@ module.exports.login = (req, res, next) => {
   })(req, res, next);
 };
 
-module.exports.update = (req, res, next) => {
-  User
-    .findById(req.payload.id)
-    .then((user) => {
-      if (!user) {
-        return res.sendStatus(401);
-      }
-      return user
-        .update(req.body.user)
-        .then(() => res.json({ user: user.toAuthJsonFor() }))
-        .catch(next);
-    })
-    .catch(next);
+module.exports.update = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.payload.id);
+    if (!user) {
+      return res.sendStatus(401);
+    }
+
+    await user.update(req.body.user);
+    return res.json({ user: user.toAuthJsonFor() });
+  } catch (err) {
+    next(err);
+  }
 };
 
-module.exports.suspend = (req, res, next) => {
-  User
-    .findById(req.payload.id)
-    .then((user) => {
-      if (!user) {
-        return res.status(401).json({ error: { message: 'You have to be an admin or a moderator to perform this action' } });
-      }
-      if (user.user_type !== 'admin' || user.user_type !== 'moderator') {
-        return res.sendStatus(401);
-      }
-      return user
-        .update({
-          suspended: true,
-          suspension_timeline: Date.now() + 21600000, // 6 hours
-        });
-    })
-    .catch(next);
+module.exports.suspend = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.payload.id);
+    if (!user) {
+      return res.status(401).json({ error: { message: 'You have to be an admin or a moderator to perform this action' } });
+    }
+    if (user.user_type !== 'admin' || user.user_type !== 'moderator') {
+      return res.sendStatus(401);
+    }
+
+    await user.update({
+      suspended: true,
+      suspension_timeline: Date.now() + 21600000, // 6 hours - default
+    });
+  } catch (err) {
+    next(err);
+  }
 };

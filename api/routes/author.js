5c820d5dc4b9876875a138d5a804ff4723771595
Nnanyielugo <kz.onyegbule@gmail.com>
2020-08-27 21:06:35 +0100

    change to author param

--- api/routes/author.js
+++ api/routes/author.js
@@ -5,8 +5,8 @@ const auth = require('../middleware/auth');
 router.param('author', author.preloadAuthor);
 router.get('/', auth.optional, author.list);
 router.post('/', auth.required, author.create);
-router.get('/:id', auth.optional, author.detail);
-router.patch('/:id', auth.required, author.update);
-router.delete('/:id', auth.required, author.delete);
+router.get('/:author', auth.optional, author.detail);
+router.patch('/:author', auth.required, author.update);
+router.delete('/:author', auth.required, author.delete);
 
 module.exports = router;

942e773e8c692593b022b83472a23c718e63844f
Nnanyielugo <kz.onyegbule@gmail.com>
2020-08-28 10:10:37 +0100

    rearrangr routes

--- api/routes/book.js
+++ api/routes/book.js
@@ -3,8 +3,8 @@ const book = require('../services/book');
 const auth = require('../middleware/auth');
 
 router.get('/', auth.optional, book.list);
-router.get('/:book', auth.optional, book.detail);
 router.post('/', auth.required, book.create);
+router.get('/:book', auth.optional, book.detail);
 router.patch('/:book', auth.required, book.update);
 router.delete('/:book', auth.required, book.delete);
 

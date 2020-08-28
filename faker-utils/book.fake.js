31d8cd789f17d036f0f8c4ec1db03fc50458d0b2
Nnanyielugo <kz.onyegbule@gmail.com>
2020-08-28 10:09:48 +0100

    book fakes and mocks

---
+++ faker-utils/book.fake.js
@@ -0,0 +1,13 @@
+const faker = require('faker');
+
+module.exports.valid_book = {
+  title: faker.lorem.sentence(6, 16),
+  summary: faker.lorem.paragraphs(7, '\n'),
+  isbn: '978-3-16-148410-0',
+};
+
+module.exports.alternate_book = {
+  title: faker.lorem.sentence(6, 16),
+  summary: faker.lorem.paragraphs(7, '\n'),
+  isbn: '988-3-16-168410-9',
+};
---
+++ test/mocks/book.js
@@ -0,0 +1,17 @@
+module.exports.valid_book = {
+  title: 'Nobis minus quod rem ut hic.',
+  summary:
+   'Quisquam magni voluptatem aliquid quod molestiae molestiae natus. Incidunt sunt necessitatibus sit dolor aut. Ipsam qui mollitia sit molestias voluptatem quia quos corporis et. Quia delectus omnis sed vero ut et ratione. Odio ad quis.\nSimilique qui itaque quibusdam consequatur debitis. Qui id aliquid accusantium sed saepe. Consectetur sed illum nobis tempore qui ratione nam aut nihil. Doloremque necessitatibus doloremque voluptatibus est ut laborum. Quisquam quas quis porro voluptates sed.\nCum vel dolores corporis recusandae recusandae fugit sed. Facilis et ratione id debitis rem repellendus. Aliquid magni rerum repellendus sint.\nPerferendis rerum voluptas sapiente omnis dolor officiis odit non. Dignissimos id itaque explicabo sunt aut occaecati ipsa. Enim rerum vitae perspiciatis placeat velit optio eveniet. Non dicta accusamus ipsa fugit vero dolorum. Voluptates magni nisi dolor animi recusandae recusandae quas numquam qui. Quod reprehenderit assumenda aut et molestiae exercitationem harum temporibus optio.\nUt et esse quod. Ut id rerum beatae saepe. Doloremque quidem aliquam corporis ea. Distinctio doloremque ea et omnis. Earum illum est incidunt dolorem.\nAb quas recusandae dignissimos ad eius qui. Impedit laudantium fugit rem. Ducimus debitis aut a animi. Consequatur voluptates suscipit excepturi magnam quos modi aut dicta repellendus. Quas aperiam qui voluptates quisquam accusamus dolorum sit. Minus velit quis quia minima.\nQuos ut necessitatibus illo ducimus earum ut nulla voluptatem veritatis. Perferendis aliquid cum qui dolorum aut quo. Officia fugit ab quis. Deserunt blanditiis sint quia id amet dicta voluptas. Adipisci ab delectus. Iure eum maiores.',
+  isbn: '978-3-16-148410-0',
+};
+
+module.exports.alternate_book = {
+  title: 'Illum aperiam rerum voluptatem est porro.',
+  summary:
+   'Et ipsam eum corporis qui ut aut sed rerum assumenda. Accusantium ea assumenda quisquam. Rerum non enim quae suscipit adipisci iste quos nulla non. Dolore maiores laborum nihil rem quibusdam.\nEst maiores et quia cupiditate aut. Tempora numquam minima numquam exercitationem qui ad sint. Numquam ad tempora quasi reiciendis repudiandae deserunt ipsa voluptas nobis.\nUt est debitis saepe dolor impedit est consequatur. Qui eaque quod. Qui expedita est inventore debitis nihil a consequatur culpa explicabo. Odit qui aspernatur neque.\nCorporis est dolor cum. Aut eius voluptatem. Quas cumque id cupiditate ipsam beatae omnis et reprehenderit soluta.\nIusto autem amet unde officia quae. Fugit nobis quasi praesentium nam itaque nobis officiis. Aspernatur dolores dicta eos molestiae magni et quia.\nConsequatur libero non voluptatum corrupti eveniet voluptatem officiis quis totam. Perferendis nobis quasi sit autem rerum. Aut nobis provident est ea sint aut quibusdam pariatur. Unde ea cumque sed ipsam deserunt consectetur ut aliquid molestias. Pariatur qui quaerat aut tenetur aspernatur molestiae doloribus quo. Recusandae officia aut aspernatur.\nAb facilis iste maxime nihil impedit natus adipisci. Voluptatum et a expedita voluptas harum qui. Earum rem minima consequatur facilis adipisci. Repudiandae veritatis perspiciatis quod perspiciatis quo.',
+  isbn: '988-3-16-168410-9',
+};
+
+module.exports.invalid_book = {
+  title: this.valid_book.title,
+};

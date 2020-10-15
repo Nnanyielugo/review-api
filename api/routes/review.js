a6a9f1a1b605fbec06df93b4372612a40a562dfd
Nnanyielugo <kz.onyegbule@gmail.com>
2020-10-15 15:18:13 +0100

    use post method for unfavorite

--- api/routes/review.js
+++ api/routes/review.js
@@ -12,13 +12,13 @@ router.post('/', auth.required, reviews.create);
 router.patch('/:review', auth.required, reviews.update);
 router.delete('/:review', auth.required, reviews.delete);
 router.post('/:review/favorite', auth.required, reviews.favorite);
-router.delete('/:review/unfavorite', auth.required, reviews.unfavorite);
+router.post('/:review/unfavorite', auth.required, reviews.unfavorite);
 
 router.get('/:review/comments', auth.optional, comments.get);
 router.post('/:review/comments', auth.required, comments.create);
 router.patch('/:review/comments/:comment', auth.required, comments.update);
 router.delete('/:review/comment/:comment', auth.required, comments.delete);
 router.post('/:review/comments/:comment/favorite', auth.required, comments.favorite);
-router.delete('/:review/comments/:comment/unfavorite', auth.required, comments.unfavorite);
+router.post('/:review/comments/:comment/unfavorite', auth.required, comments.unfavorite);
 
 module.exports = router;

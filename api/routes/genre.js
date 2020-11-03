e6a770583e5887a755ebce99b2d0a6704c0a49b5
Nnanyielugo <kz.onyegbule@gmail.com>
2020-11-03 17:37:12 +0100

    add preload

--- api/routes/genre.js
+++ api/routes/genre.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
-const genre = require('../services/genre');
+const genres = require('../services/genre');
 const auth = require('../middleware/auth');
 
-router.get('/', auth.optional, genre.list);
-router.get('/:genre', auth.optional, genre.detail);
-router.post('/', auth.required, genre.create);
-router.patch('/:genre', auth.required, genre.update);
-router.delete('/:genre', auth.required, genre.delete);
+router.param('genre', genres.preloadGenre);
+router.get('/', auth.optional, genres.list);
+router.get('/:genre', auth.optional, genres.detail);
+router.post('/', auth.required, genres.create);
+router.patch('/:genre', auth.required, genres.update);
+router.delete('/:genre', auth.required, genres.delete);
 
 module.exports = router;
